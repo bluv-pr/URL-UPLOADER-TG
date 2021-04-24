45b68da17843f00357fb3b80312911bdbbf1d9c4
Balaji Shinnosuke <62749324+bluv-pr@users.noreply.github.com>
2021-04-24 19:05:01 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -34,8 +34,41 @@ def GetExpiryDate(chat_id):
 @pyrogram.Client.on_message(pyrogram.Filters.command(["help", "about"]))
 async def help_user(bot, update):
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/help")
-    await bot.send_message(
+    #TRChatBase(update.from_user.id, update.text, "/help")
+    update_channel = Config.UPDATE_CHANNEL
+
+    if update_channel:
+
+        try:
+
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+
+            if user.status == "kicked":
+
+               await update.reply_text(" Sorry, You are **B A N N E D**")
+
+               return
+
+        except UserNotParticipant:
+
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
+
+            await update.reply_text(
+
+                text="**Oh Dear In Order To Use Me Join My Update Channnl 🤭**",
+
+                reply_markup=InlineKeyboardMarkup([
+
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+
+              ])
+
+            )
+
+            return
+
+        else:
+            await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.HELP_USER,
         parse_mode="html",
@@ -47,9 +80,24 @@ async def help_user(bot, update):
 @pyrogram.Client.on_message(pyrogram.Filters.command(["me"]))
 async def get_me_info(bot, update):
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/me")
-    chat_id = str(update.from_user.id)
-    chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
+    #TRChatBase(update.from_user.id, update.text, "/me")
+    update_channel = Config.UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked":
+               await update.reply_text(" Sorry, You are **B A N N E D**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
+            await update.reply_text(
+                text="**Oh Dear In Order To Use Me Join My Update Channnl 🤭**",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+              ])
+            )
+            return
+        else:
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.CURENT_PLAN_DETAILS.format(chat_id, plan_type, expires_at),
@@ -62,8 +110,41 @@ async def get_me_info(bot, update):
 @pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
 async def start(bot, update):
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/start")
-    await bot.send_message(
+    #TRChatBase(update.from_user.id, update.text, "/start")
+    update_channel = Config.UPDATE_CHANNEL
+
+    if update_channel:
+
+        try:
+
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+
+            if user.status == "kicked":
+
+               await update.reply_text(" Sorry, You are **B A N N E D**")
+
+               return
+
+        except UserNotParticipant:
+
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
+
+            await update.reply_text(
+
+                text="**Oh Dear In Order To Use Me Join My Update Channnl 🤭**",
+
+                reply_markup=InlineKeyboardMarkup([
+
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+
+              ])
+
+            )
+
+            return
+
+        else:
+          await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
         reply_to_message_id=update.message_id
@@ -73,8 +154,41 @@ async def start(bot, update):
 @pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
 async def upgrade(bot, update):
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/upgrade")
-    await bot.send_message(
+    #TRChatBase(update.from_user.id, update.text, "/upgrade")
+    update_channel = Config.UPDATE_CHANNEL
+
+    if update_channel:
+
+        try:
+
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+
+            if user.status == "kicked":
+
+               await update.reply_text(" Sorry, You are **B A N N E D**")
+
+               return
+
+        except UserNotParticipant:
+
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
+
+            await update.reply_text(
+
+                text="**Oh Dear In Order To Use Me Join My Update Channnl 🤭**",
+
+                reply_markup=InlineKeyboardMarkup([
+
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+
+              ])
+
+            )
+
+            return
+
+        else:
+           await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.UPGRADE_TEXT,
         parse_mode="html",
